ee143b63a3942e1261d6e60c1891b848495112d9
crraxx <crraxx@163.com>
2021-01-30 19:49:28 +0800

    javaSE-day1

---
+++ Day1/Text1.java
@@ -0,0 +1,26 @@
+package com.ff.javabase.Day1;
+
+public class Text1 {
+    //单行注释，对某行代码进行说明 Ctrl+/
+    //main
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello world！");
+
+       /*
+       多行注释
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        */
+
+
+
+        System.out.println(dom.num);
+        dom.eat();
+        dom.drink();
+
+    }
+}
---
+++ Day1/dom.java
@@ -0,0 +1,24 @@
+package com.ff.javabase.Day1;
+            /**
+             * 这是一个文档注释演示
+             */
+            public class dom{
+            /**
+             * 这是一个计数器
+             */
+            static int num;
+            /**
+             * 这是一个吃饭的方法
+             */
+            public static void eat(){
+
+            }
+
+            /**
+             * 这是一个喝的类
+             */
+            public static void drink(){
+
+            }
+    }
+
