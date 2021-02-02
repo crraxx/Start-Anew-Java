89264e276d2e00651c6829e553c289f2a74d1135
crraxx <78258888+crraxx@users.noreply.github.com>
2021-02-02 22:31:12 +0800

    javase

---
+++ Day4/MethedDemo.java
@@ -0,0 +1,107 @@
+package com.ff.java.Day4;
+import java.util.Scanner;
+public class MethedDemo {
+   public static void main(String[] args) {
+     /*   Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        if (x>0&&x <= 2) {
+            switch (x) {
+                case 1:
+                    System.out.println("10.5");
+                    break;
+                case 2:
+                    System.out.println("21");
+                    break;
+            }
+        }
+        else if(x>2) {
+            System.out.println(21 + (x - 2) * 4);
+        }
+        else{
+            System.out.println("输入为负数，不合法");
+        }
+
+    }
+}*/
+       /* s.open();
+        s.open();*/
+      //  System.out.println( s.getMax(5,10,15));
+//        Scanner input=new Scanner(System.in);
+//        int x=input.nextInt();
+//        int y=input.nextInt();
+//        int z=input.nextInt();
+//        System.out.println( s.getMax(x,y,z));
+//    }
+
+
+   /* public void open() {
+        System.out.println("开门");
+    }*/
+
+//    public int getMax(int x,int y,int z){
+//        int max;
+//        return ( max=(x>y)? x:y)>z ? max :z;
+//    }
+
+
+    /*public void test() {
+
+        for (int a = 1; a <= 5; a++) {
+            if (a == 5) {
+                return;
+            }
+        }
+
+}*/
+
+   /* public static void main(String[] args) {
+        show(9);
+    }
+    public static void show(int a) {
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(j + "*" + i + "=" + j * i + "\t");
+            }
+            System.out.println();
+        }
+    }*/
+   /*public static void main(String[] args) {
+       show(5);
+   }
+   public static void show (int a){
+       for (int i = 0; i < a; i++) {
+           for (int j = 0; j < a; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+   }*/
+
+
+/*public class MethedDemo {
+    public void main(String[] args) {*/
+      /*  MethedDemo s = new MethedDemo();
+        Scanner input=new Scanner(System.in);
+        int x=input.nextInt();
+        int y=input.nextInt();
+        int z=input.nextInt();
+        System.out.println( s.getMax(x,y,z));
+    }
+
+    public int getMax(int x,int y,int z){
+        int max;
+        return ( max=(x>y)? x:y)>z ? max :z;
+    }*/
+       show(5);
+   }
+    public static void show (int a){
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
